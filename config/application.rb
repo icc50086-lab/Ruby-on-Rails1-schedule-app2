66383dcd25c9066c9452d1c83052bcc1f3a6db7d
icc50086-lab <icc50086@gmail.com>
2026-02-17 14:14:30 +0900

    Fix: set load_defaults to 7.2 and timezone to Tokyo

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,6 @@ module EmptyApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.active_record.default_timezone = :local
   end
 end
