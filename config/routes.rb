d981d80e6dec7f3b9380bac43252d4604b2ba55e
icc50086-lab <icc50086@gmail.com>
2026-02-18 13:58:25 +0900

    Fix indentation

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,8 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
-   root "schedules#index"
+     # root "posts#index"
+      root "schedules#index"
+
    resources :schedules
 end
